70fc1bfa866318d7a5e53bc67ddd56e1c3f546e2
krishnb <kbandame@gmail.com>
2021-04-05 18:09:58 +0530

    Added MDB Test

--- src/main/java/com/testvagrant/optimusLite/commons/entities/DeviceDetails.java
+++ src/main/java/com/testvagrant/optimusLite/commons/entities/DeviceDetails.java
@@ -34,54 +34,6 @@ public class DeviceDetails {
     private Status status;
     private String udid;
 
-    public String getDeviceName() {
-        return deviceName;
-    }
-
-    public void setDeviceName(String deviceName) {
-        this.deviceName = deviceName;
-    }
-
-    public Platform getPlatform() {
-        return platform;
-    }
-
-    public void setPlatform(Platform platform) {
-        this.platform = platform;
-    }
-
-    public String getPlatformVersion() {
-        return platformVersion;
-    }
-
-    public void setPlatformVersion(String platformVersion) {
-        this.platformVersion = platformVersion;
-    }
-
-    public DeviceType getRunsOn() {
-        return runsOn;
-    }
-
-    public void setRunsOn(DeviceType runsOn) {
-        this.runsOn = runsOn;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
-
-    public String getUdid() {
-        return udid;
-    }
-
-    public void setUdid(String udid) {
-        this.udid = udid;
-    }
-
     @Override
     public String toString() {
         return "DeviceDetails{" +
