e930c1b5c7b003009bd279a74d9c5e58062746e2
krishnb <kbandame@gmail.com>
2021-04-27 18:15:44 +0530

    Completed screenshot capture logic:

---
+++ src/main/java/com/testvagrant/optimus/core/screenshots/OptimusRunTarget.java
@@ -0,0 +1,102 @@
+package com.testvagrant.optimus.core.screenshots;
+
+import com.testvagrant.optimus.commons.filehandlers.GsonParser;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.logging.LogEntries;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public class OptimusRunTarget {
+
+  private OptimusRunContext optimusRunContext;
+  private String screenshotDirPath, logDirPath;
+
+  public OptimusRunTarget(OptimusRunContext optimusRunContext) {
+    this.optimusRunContext = optimusRunContext;
+    screenshotDirPath = createScreenShotDirectory(optimusRunContext);
+    logDirPath = createLogsDirectory(optimusRunContext);
+    saveTargetDetails();
+  }
+
+  public void captureScreenshot() {
+    File file = takeScreenshot();
+    Path desitnationPath = Paths.get(optimusRunContext.getTestFolder().toString(),
+            "screenshots",
+            LocalDateTime.now().toString()+".png");
+    try {
+      Files.move(file.toPath(), desitnationPath, StandardCopyOption.REPLACE_EXISTING);
+    } catch (IOException e) {
+      //cannot move screenshot
+    }
+  }
+
+  public void captureLogs() {
+    Set<String> availableLogTypes = null;
+    try {
+      availableLogTypes = optimusRunContext.getWebDriver().manage().logs().getAvailableLogTypes();
+    } catch (Exception e) {
+      return;
+    }
+    availableLogTypes.forEach(logType -> {
+      LogEntries logEntries = optimusRunContext.getWebDriver().manage().logs().get(logType);
+      FileWriter file = null;
+      try {
+        file = new FileWriter(Paths.get(logDirPath, logType+".json").toString());
+        file.write(String.valueOf(logEntries.toJson()));
+        file.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    });
+  }
+
+  private void saveTargetDetails() {
+    String serialize = GsonParser.toInstance().serialize(optimusRunContext.getTargets());
+    FileWriter file = null;
+    try {
+      file = new FileWriter(Paths.get(optimusRunContext.getTestFolder().toString(), "target.json").toString());
+      file.write(serialize);
+      file.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private String createScreenShotDirectory(OptimusRunContext optimusRunContext) {
+    Path screenshotDirPath = Paths.get(optimusRunContext.getTestFolder().toString(), "screenshots");
+    return createDirectory(screenshotDirPath);
+  }
+
+  private String createLogsDirectory(OptimusRunContext optimusRunContext) {
+    Path logDirPath = Paths.get(optimusRunContext.getTestFolder().toString(),
+            "logs");
+    return createDirectory(logDirPath);
+  }
+
+  private String createDirectory(Path dirPath) {
+    File file = new File(dirPath.toString());
+    if(file.exists() && file.isDirectory()) {
+      try {
+        FileUtils.deleteDirectory(file);
+      } catch (IOException e) {
+        // ignore exception on delete directory
+      }
+    }
+    if(!file.exists()) file.mkdirs();
+    return file.getAbsolutePath();
+  }
+
+  private File takeScreenshot() {
+    return ((TakesScreenshot) optimusRunContext.getWebDriver()).getScreenshotAs(OutputType.FILE);
+  }
+}
--- src/test/java/com/testvagrant/optimus/core/WebDriverTest.java
+++ src/test/java/com/testvagrant/optimus/core/WebDriverTest.java
@@ -1,6 +1,10 @@
 package com.testvagrant.optimus.core;
 
+import com.testvagrant.optimus.commons.entities.Device;
+import com.testvagrant.optimus.core.screenshots.OptimusRunTarget;
+import com.testvagrant.optimus.core.screenshots.OptimusRunContext;
 import com.testvagrant.optimus.core.web.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -20,6 +24,31 @@ public class WebDriverTest {
     System.setProperty("testFeed", "firefoxTestFeed");
     WebDriver driver = new WebDriverManager().createDriver();
     Assert.assertNotNull(driver);
+  }
+
+  @Test
+  public void screenshotTest() throws InterruptedException {
+    System.setProperty("testFeed", "chromeTestFeed");
+    WebDriver driver = new WebDriverManager().createDriver();
+    Device device = Device.builder().deviceName("Chrome")
+            .platformVersion("90.2").build();
+    Device device1 = Device.builder().deviceName("SamsungJ2")
+            .platformVersion("10.2").build();
+
+    driver.get("http://www.google.com");
+    OptimusRunContext optimusRunContext =
+        OptimusRunContext.builder()
+            .webDriver(driver)
+            .build()
+            .addTarget(device)
+                .addTarget(device1)
+            .testPath("WebDriverTest", "screenshotTest");
+    OptimusRunTarget optimusRunTarget = new OptimusRunTarget(optimusRunContext);
+    optimusRunTarget.captureScreenshot();
+    Thread.sleep(2000);
+    driver.findElement(By.name("q")).sendKeys("Hello taking a screenshot");
+    optimusRunTarget.captureScreenshot();
+    optimusRunTarget.captureLogs();
   }
 
   @AfterMethod
