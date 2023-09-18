02a40c9293b9494bd397f39062d6c820d2174a5e
Prajwalgn-07 <prajwalg@testvagrant.com>
2023-09-18 08:59:47 +0530

    used streams

--- src/main/java/com/testvagrant/ekam/drivers/mobile/CapabilityMapper.java
+++ src/main/java/com/testvagrant/ekam/drivers/mobile/CapabilityMapper.java
@@ -2,17 +2,10 @@ package com.testvagrant.ekam.drivers.mobile;
 
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.util.Map;
-
 public class CapabilityMapper<T extends MutableCapabilities> {
 
     public T mapToOptions(DesiredCapabilities desiredCapabilities, T options) {
-        for (Map.Entry<String, Object> entry : desiredCapabilities.asMap().entrySet()) {
-            String key = entry.getKey();
-            Object value = entry.getValue();
-            options.setCapability(key, value);
-        }
+        desiredCapabilities.asMap().forEach(options::setCapability);
         return options;
     }
 }
